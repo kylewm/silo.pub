11827029a85f194aeadcb1895b272b8c9e9e5c22
Kyle Mahan <kyle.mahan@gmail.com>
2015-04-14 08:13:47 -0700

    redirect all requests to HTTPS

--- feverdream/app.py
+++ feverdream/app.py
@@ -1,16 +1,30 @@
 from flask import Flask
+from flask_sslify import SSLify
 from feverdream.views import views
 from feverdream.wordpress import wordpress
 from feverdream.blogger import blogger
 from feverdream.tumblr import tumblr
 from feverdream.micropub import micropub
 from feverdream import extensions
+import logging
 from feverdream.models import *
 
 
 def create_app(config_path):
     app = Flask(__name__)
     app.config.from_pyfile(config_path)
+
+    if not app.debug:
+        app.logger.setLevel(logging.DEBUG)
+        stream_handler = StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stream_handler.setFormatter(formatter)
+        app.logger.addHandler(stream_handler)
+
+    # redirect all requests to https on Heroku
+    if 'DYNO' in os.environ:
+        SSLify(app)
     extensions.init_app(app)
     app.register_blueprint(views)
     app.register_blueprint(wordpress)
