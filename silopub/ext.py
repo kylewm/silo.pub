0fdefd138b948e429c6b256d7021aef8105a0c59
Kyle Mahan <kyle.mahan@gmail.com>
2015-11-22 11:28:04 -0800

    Remove fancy Alembic/Migrations stuff

--- silopub/ext.py
+++ silopub/ext.py
@@ -1,12 +1,10 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.migrate import Migrate
 from flask.ext.wtf import CsrfProtect
 from flask.ext.session import Session
 from redis import StrictRedis
 
 
 db = SQLAlchemy()
-migrate = Migrate()
 csrf = CsrfProtect()
 sess = Session()
 redis = StrictRedis()
@@ -14,6 +12,5 @@ redis = StrictRedis()
 
 def init_app(app):
     db.init_app(app)
-    migrate.init_app(app, db)
     csrf.init_app(app)
     sess.init_app(app)
